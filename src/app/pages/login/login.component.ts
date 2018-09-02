135c815a2db7939e888ee0e165b0a4f47f1e3dfb
Theerasak Tubrit <jigkoh3@Theerasaks-MacBook-Pro.local>
2018-09-02 22:00:29 +0700

    [edit] login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
   };
 
   constructor(
-    private restApiService : RestApiService,
+    private restApiService: RestApiService,
     private router: Router
   ) { }
 
@@ -23,8 +23,8 @@ export class LoginComponent implements OnInit {
   }
 
   formValid() {
-    let isValid:boolean = this.credential && this.credential.username && this.credential.password ? true : false;
-    
+    let isValid: boolean = this.credential && this.credential.username && this.credential.password ? true : false;
+
     return isValid;
   }
 
@@ -32,19 +32,13 @@ export class LoginComponent implements OnInit {
     try {
       let loginResponse = await this.restApiService.post(Constants.URL() + '/api/auth/signin', this.credential);
       if (loginResponse['status'] === 200) {
-        try {
-          window.localStorage.setItem('token-admin@schoolhub', loginResponse['token']);
-          this.router.navigate(['/home']);
-        } catch (error) {
-          if (error) {
-            //this.dataService.error('โหลดข้อมูลผู้ใช้ล้มเหลว');
-          }
-        }
-      }
-    } catch (error) {
-      if (error) {
+        window.localStorage.setItem('token-admin@schoolhub', loginResponse['token']);
+        this.router.navigate(['/home']);
+      }else{
         //this.dataService.error('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
       }
+    } catch (error) {
+      //this.dataService.error('ไม่สามารถเชื่อมต่อข้อมูลได้');
     }
   }
 
